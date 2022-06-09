3769eacf9db08b47f545c5b290ad4e185e989cea
tiheryv <tiheryvr@gmail.com>
2022-06-08 20:31:46 -0500

    Se agregan estilos en registro.php

--- registro.php
+++ registro.php
@@ -9,13 +9,13 @@ if (isset($_POST['submit'])) {
 		$correo = $con->real_escape_string($_POST['correo']);
 		$contrasenia = $con->real_escape_string($_POST['contrasenia']);
 		$cContrasenia = $con->real_escape_string($_POST['cContrasenia']);
-		# code...
+	
 		if ($contrasenia != $cContrasenia)
-            $msg = "Checa tus contraseñas";
+            $msg = "Ups, parece que que hay un error con tu contraseña.";
         else {
             $hash = password_hash($contrasenia, PASSWORD_BCRYPT);
             $con->query("INSERT INTO usuarios (nombre,correo,contrasenia) VALUES ('$nombre','$correo','$hash')");
-            $msg = "Ya quedaste registrado";
+            $msg = "Registro exitoso";
         }
 	}
 ?>
@@ -27,8 +27,12 @@ if (isset($_POST['submit'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maxium-scale=1.0, minimo-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>PHP Hashing</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Registro</title>
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+        crossorigin="anonymous">
 </head>
 <body>
 <div class="container" style="margin-top: 100px;">
@@ -39,12 +43,11 @@ if (isset($_POST['submit'])) {
             <?php if($msg != "") echo $msg . "<br><br>"; ?>
 
             <form method="post" action="registro.php">
-                <input class="form-control" minlength="3" name="nombre" placeholder="Nombre..."><br>
-                <input class="form-control" name="correo" placeholder="Correo..."><br>
+                <input class="form-control" minlength="3" name="nombre" placeholder="Ingrese su nombre..."><br>
+                <input class="form-control" name="correo" placeholder="Ingrese su correo..."><br>
                 <input class="form-control" minlength="5" name="contrasenia" type="password" placeholder="Contraseña..."><br>
                 <input class="form-control" minlength="5" name="cContrasenia" type="password" placeholder="Confirmar contraseña..."><br>
-                <input class="btn btn-primary" name="submit" type="submit" value="Registrarse..."><br>
-
+                <input class="btn btn-secondary" name="submit" type="submit" value="Registrarse"><br>
             </form>
 </body>
 </html>
